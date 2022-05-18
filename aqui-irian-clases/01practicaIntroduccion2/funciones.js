0afe6f351679bdd3b16f2af0a18f2d2ff78f410b
JoseChavez <2410bautista@gmail.com>
2022-05-17 21:36:29 -0500

    prueba fechas

---
+++ aqui-irian-clases/01practicaIntroduccion2/funciones.js
@@ -0,0 +1,25 @@
+function calcular(n1, n2, operador)
+{
+    switch (operador) {
+        case '+':
+            return n1+n2;
+        case '-':
+            return n1-n2;
+        case '*':
+            return n1*n2;
+        case '/':
+            return n1/n2;
+        default:
+            return 0;
+    }
+}
+function saludar(nombre){
+    console.log(`hola ${nombre}`)
+}
+
+module.exports = {
+    variable: 100,
+    funcion1: calcular,
+    saludar
+};
+
